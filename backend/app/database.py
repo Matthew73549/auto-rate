6ea0fe69d931d1496e8eb67682ece8ff15b87113
Matthew73549 <akhmed.mikhaltsov@inbox.ru>
2026-09-13 11:23:06 +0300

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,6 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print(f"DEBUG: DATABASE_URL = {DATABASE_URL}")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set! Check Environment Variables in Render!")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
