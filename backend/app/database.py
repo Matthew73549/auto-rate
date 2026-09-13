2f3d2be48cba0499ddf14415c533b1982a29a110
Matthew73549 <akhmed.mikhaltsov@inbox.ru>
2026-09-13 12:52:56 +0300

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,12 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://auto_rate_db_user:IZetKlLWiQNI9TEDCx2lYvz6YhH81Rnh@dpg-daj5dqmq1p3s73a6m6e0-a/auto_rate_db"
 
 print(f"DEBUG: DATABASE_URL = {DATABASE_URL}")
-
-if DATABASE_URL is None:
-    raise ValueError("DATABASE_URL is not set! Check Environment Variables in Render!")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
